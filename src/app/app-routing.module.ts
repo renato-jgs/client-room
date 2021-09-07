ae9d8859d22a514dd6784e2b8820d145f192f81a
Renato Santana <renato_jgs@hotmail.com>
2021-09-07 12:17:42 -0300

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { RoomDetailsComponent } from './room-details/room-details.component';
+import { UpdateRoomComponent } from './update-room/update-room.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { RoomListComponent } from './room-list/room-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'rooms', pathMatch: 'full' },
+  { path: 'rooms', component: RoomListComponent },
+  { path: 'add', component: CreateRoomComponent },
+  { path: 'update/:id', component: UpdateRoomComponent },
+  { path: 'details/:id', component: RoomDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
